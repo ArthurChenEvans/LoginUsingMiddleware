da930e2b01d14ffbf56384d072f0831a161dd801
Arthur Evans <arthur.evans@pdms.com>
2024-02-26 13:15:30 +0000

    Added injection and conditional for task requirements

--- LoginUsingMiddleware/LoginUsingMiddleware/Program.cs
+++ LoginUsingMiddleware/LoginUsingMiddleware/Program.cs
@@ -2,7 +2,24 @@ using LoginUsingMiddleware.CustomMiddlewares;
 
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<LoginMiddleware>();  
 var app = builder.Build();
 
+app.UseWhen(
+    context => context.Request.Method == "POST",
+    app =>
+    {
+        app.UseLogin();
+    });
+
+app.UseWhen(
+    context => context.Request.Method == "GET",
+    app => app.Run(async context =>
+    {
+        context.Response.StatusCode = 200;
+        await context.Response.WriteAsync("No response");
+    })
+);
+    
 app.UseLogin();
 app.Run();
