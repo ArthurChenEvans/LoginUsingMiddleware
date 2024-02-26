01f7eddd471a8404bb981029a92c55d9189a09d1
Arthur Evans <arthur.evans@pdms.com>
2024-02-26 12:35:11 +0000

    Created LoginMiddleware and logic

---
+++ LoginUsingMiddleware/LoginUsingMiddleware/CustomMiddlewares/LoginMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace LoginUsingMiddleware.CustomMiddlewares
+{
+    public class LoginMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            StreamReader reader = new StreamReader(context.Request.Body);
+
+            var body = await reader.ReadToEndAsync();
+
+            Dictionary<string, StringValues> queryParams =
+                Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
+
+
+            if (queryParams != null)
+            {
+                bool emailIsValid = false;
+                bool passwordIsValid = false;
+
+                foreach (var kvp in queryParams)
+                {
+                    if (kvp.Key == "email" && kvp.Value == "admin@example.com")
+                    {
+                        emailIsValid = true;
+                    }
+                    else if (kvp.Key == "password" && kvp.Value == "admin1234")
+                    {
+                        passwordIsValid = true;
+                    }
+                }
+
+                if (emailIsValid && passwordIsValid)
+                {
+                    context.Response.StatusCode = 200;
+                    await context.Response.WriteAsync("Successful login");
+                }
+                else
+                {
+                    context.Response.StatusCode = 403;
+                    await context.Response.WriteAsync("Invalid login");
+                }
+            }
+
+            await next(context);
+        }
+
+    }
+}
